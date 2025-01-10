46b373537f6d3dbd0ee340807e4ed0fc9a02d56e
1810Biswa <deyb18860@gamil.com>
2025-01-10 20:30:09 +0530

    Add Tea class with comparison functionality and display method

---
+++ OOP/friend.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+class Tea
+{
+
+private:
+    string teaName;
+    int servings;
+
+public:
+    Tea(string name, int serve) : teaName(name), servings(serve) {} // contructor
+
+    friend bool compareServe(const Tea &tea1, const Tea &tea2);
+
+    void display() const
+    {
+        cout << "Here is :  " << teaName << "No. of cup :" << endl;
+    }
+};
+
+bool compareServe(const Tea &tea1, const Tea &tea2)
+{
+    cout << (tea1.servings > tea2.servings) <<endl ;
+   return tea1.servings > tea2.servings;
+
+}
+
+int main()
+{
+
+    Tea masalaTea("Masala Tea", 8);
+    Tea gingerTea("ginger Tea", 3);
+
+    if (compareServe(masalaTea, gingerTea))
+    {
+        cout << "Masala tea is having MORE servings" << endl;
+    }
+    else
+    {
+        cout << "Masala tea is having LESS servings" << endl;
+    }
+
+    return 0;
+}
